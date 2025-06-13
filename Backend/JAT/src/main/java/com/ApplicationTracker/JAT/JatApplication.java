a7cd4662866d0ef193c05a0fae59b007e5400b81
dhirajreddy2001 <dhirajreddy2001@gmail.com>
2025-06-13 15:00:06 -0500

    Project Set-up

---
+++ Backend/JAT/src/main/java/com/ApplicationTracker/JAT/JatApplication.java
@@ -0,0 +1,13 @@
+package com.ApplicationTracker.JAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatApplication.class, args);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Job pplication Tracker Project
